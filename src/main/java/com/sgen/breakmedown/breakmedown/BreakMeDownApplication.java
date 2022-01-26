3fcaa5c95cb97ef2dd0b82159c901991cda9841d
sgenlecroyant <sgenlecroyant@gmail.com>
2022-01-26 23:37:13 +0200

    app set-up

---
+++ src/main/java/com/sgen/breakmedown/breakmedown/BreakMeDownApplication.java
@@ -0,0 +1,15 @@
+package com.sgen.breakmedown.breakmedown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakMeDownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreakMeDownApplication.class, args);
+		
+		System.out.println("Break-Me-Down If you can!");
+	}
+
+}
