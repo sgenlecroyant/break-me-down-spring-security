4bf94bc766184a852fff584e8c331bd3487e6929
sgenlecroyant <sgenlecroyant@gmail.com>
2022-01-28 17:55:40 +0200

    added the devTools and the Java Mail Sender dependencies

--- src/main/java/com/sgen/breakmedown/breakmedown/BreakMeDownApplication.java
+++ src/main/java/com/sgen/breakmedown/breakmedown/BreakMeDownApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BreakMeDownApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BreakMeDownApplication.class, args);
-		
 		System.out.println("Break-Me-Down If you can!");
 	}
 
---
+++ src/main/java/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http
+			.authorizeHttpRequests()
+			.antMatchers("/**").permitAll();
+	}
+
+	
+}
