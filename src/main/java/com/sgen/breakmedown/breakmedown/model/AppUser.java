5f9f6c5aa30779684fc65e0882d9c9888ec55e2e
sgenlecroyant <sgenlecroyant@gmail.com>
2022-01-28 20:26:26 +0200

    created the entity table

---
+++ src/main/java/com/sgen/breakmedown/breakmedown/model/AppUser.java
@@ -0,0 +1,76 @@
+package com.sgen.breakmedown.breakmedown.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity(name = "AppUsers")
+@Table(name = "AppUsers")
+public class AppUser {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String firstName;
+	private String lastName;
+	private String username;
+	private String password;
+	
+	public AppUser() {
+		// default constructor for database mapping
+	}
+
+	public AppUser(String firstName, String lastName, String username, String password) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+		this.password = password;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
+	
+}
