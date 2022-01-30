b5b231da08a96e69bc59ec28c2701c7d3f93857e
sgenlecroyant <sgenlecroyant@gmail.com>
2022-01-30 23:41:36 +0200

    create the assign subscription API

--- src/main/java/com/sgen/breakmedown/breakmedown/controller/appusersubscription/AppUserSubscriptionController.java
+++ src/main/java/com/sgen/breakmedown/breakmedown/controller/appusersubscription/AppUserSubscriptionController.java
@@ -2,9 +2,12 @@ package com.sgen.breakmedown.breakmedown.controller.appusersubscription;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sgen.breakmedown.breakmedown.model.AppUser;
 import com.sgen.breakmedown.breakmedown.service.AppUserService;
 import com.sgen.breakmedown.breakmedown.service.SubscriptionService;
 
@@ -18,6 +21,11 @@ public class AppUserSubscriptionController {
 	@Autowired
 	private SubscriptionService subscriptionService;
 	
-//	public ResponseEntity<String> 
+	@PostMapping(value = "/assign/{appUserId}/sub/{subsId}")
+	public ResponseEntity<String> assignSubscription(@PathVariable Integer appUserId, @PathVariable Integer subsId){
+		
+		this.appUserService.findAppUserById(appUserId);
+		return null;
+	}
 
 }
