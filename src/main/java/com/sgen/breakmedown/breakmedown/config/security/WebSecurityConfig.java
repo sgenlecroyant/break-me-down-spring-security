0c8e4f0b85cadcc2f8cd8ef07f8f1ebcbd298624
sgenlecroyant <sgenlecroyant@gmail.com>
2022-07-05 12:27:45 +0200

    permitted all requests

--- src/main/java/com/sgen/breakmedown/breakmedown/config/security/WebSecurityConfig.java
+++ src/main/java/com/sgen/breakmedown/breakmedown/config/security/WebSecurityConfig.java
@@ -17,7 +17,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 				.authorizeRequests()
 				.anyRequest().permitAll()
 				.and()
-				.formLogin();
+				.formLogin()
+				;
 	}
 	
 	
