daa1afe55734b2c2dc20c881b4982bef8eec1cd3
sgenlecroyant <sgenlecroyant@gmail.com>
2022-07-05 14:03:03 +0200

    tested deleteById SUCCESS

--- src/test/java/com/sgen/breakmedown/breakmedown/service/AppUserSubscriptionServiceTest.java
+++ src/test/java/com/sgen/breakmedown/breakmedown/service/AppUserSubscriptionServiceTest.java
@@ -5,11 +5,13 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 
 import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -21,45 +23,52 @@ import com.sgen.breakmedown.breakmedown.repository.SubscriptionRepo;
 import com.sgen.breakmedown.breakmedown.subscriptionType.SubscriptionType;
 
 @SpringBootTest
+//formatter:off
 class AppUserSubscriptionServiceTest {
-	
+
 	@Mock
 	private SubscriptionRepo subscriptionRepo;
-	
+
 	@InjectMocks
 	private SubscriptionService subscriptionService;
 
 	@Test
 	public void testFetchAllSubscriptionsSuccess() {
-		List<Subscription> subscriptions = List.of(
-				new Subscription(SubscriptionType.S4G, "4G Net", 1200f),
+		List<Subscription> subscriptions = List.of(new Subscription(SubscriptionType.S4G, "4G Net", 1200f),
 				new Subscription(SubscriptionType.S4G, "3G Net", 1000f));
 		Mockito.when(this.subscriptionRepo.findAll()).thenReturn(subscriptions);
 		List<Subscription> allSubscriptions = this.subscriptionService.fetchAllSubscriptions();
-		
+
 		Mockito.verify(this.subscriptionRepo).findAll();
-		
+
 		assertThat(allSubscriptions.size()).isEqualTo(subscriptions.size());
-		
+
 	}
-	
+
 	@Test
 	public void testFetchAllSubscriptionsFailure() {
-		List<Subscription> subscriptions = List.of(
-				new Subscription(SubscriptionType.S4G, "4G Net", 1200f),
+		List<Subscription> subscriptions = List.of(new Subscription(SubscriptionType.S4G, "4G Net", 1200f),
 				new Subscription(SubscriptionType.S4G, "3G Net", 1000f));
 		List<Subscription> allSubscriptions = this.subscriptionService.fetchAllSubscriptions();
-		
+
 		assertThat(allSubscriptions.size()).isLessThan(subscriptions.size());
 	}
-	
+
 	@Test
 	public void testDeleteSubscriptionByIdFailure() {
 		Mockito.when(this.subscriptionRepo.findById(anyInt())).thenReturn(Optional.empty());
 		Mockito.verify(this.subscriptionRepo, never()).delete(any());
-		
+
 		assertThatThrownBy(() -> this.subscriptionService.deleteSubscriptionById(anyInt()))
-							.hasMessage("subscription not available");
-				
+				.hasMessage("subscription not available");
+
+	}
+
+	@Test
+	public void testDeleteSubscriptionByIdSuccess() {
+		BDDMockito.given(this.subscriptionRepo.findById(anyInt()))
+				.willReturn(Optional.of(new Subscription(SubscriptionType.S4G, "4G Net", 1200f)));
+		this.subscriptionService.deleteSubscriptionById(anyInt());
+		Mockito.verify(this.subscriptionRepo, times(1)).deleteById(anyInt());
 	}
 }
